1423b472105b6b0eb1b5d06141f30d916fab05dc
kunal das <wastematerial12345@gmail.com>
2024-11-05 13:48:42 +0530

    test code

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,14 @@
+import Chai from "./chai"
+// importing Chai function in App function
+
 function App() {
 
   return (
-    <h1>Chai aur react with vite | KD</h1>
+    <> 
+      <Chai />
+      <h1>Chai aur React with vite</h1>
+      <p>This a para</p>
+    </> // The empty tag is called fragment tag
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Chai from './chai'
+// we are import Chai function as well as App function
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+   <>
+    <Chai/>
+    <App/>
+   </>
+  
 )
