ac29fa6a9c0f8bfc03b89188952440a06329faa6
kunal das <wastematerial12345@gmail.com>
2024-11-06 19:17:53 +0530

    counter project

---
+++ 02counterProject/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [count,setCount] = useState(10)
+
+  const addValue=() => {
+      if(count == 20){
+        alert("Counter value not exceed 20")
+      }else{
+        setCount(count + 1)
+      }
+  }
+
+  const removeValue = () => {
+    if(count == 0){
+      alert("Counter value not accepts negative value")
+    }else{
+      setCount(count - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Counter Project Using Hooks</h1>
+      <h2>Counter : {count}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
