6f0f4a9c5b506930b0de8c8c741ffc87b0a41cce
kunal das <wastematerial12345@gmail.com>
2025-01-04 15:51:02 +0530

    IDK

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,15 @@
+import UserContextProvider from './context/UserContextProvider'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>React with chai</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
